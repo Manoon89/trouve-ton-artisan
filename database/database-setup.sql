c87597a8e0828fbf52012ea147a92764d4377344
Manoon <manon.lagniel@gmail.com>
2025-04-09 08:30:20 +0200

    complete database-setup

--- database/database-setup.sql
+++ database/database-setup.sql
@@ -17,3 +17,43 @@ CREATE TABLE IF NOT EXISTS `ville` (
     `nom_ville` VARCHAR(45) NOT NULL, 
     PRIMARY KEY (`id_ville`)
 ) ENGINE=InnoDB ;
+
+-- On crée la table catégorie
+CREATE TABLE IF NOT EXISTS `categorie` (
+    `id_categorie` INT NOT NULL UNIQUE AUTO_INCREMENT, 
+    `nom_categorie` VARCHAR(20) NOT NULL UNIQUE, 
+    PRIMARY KEY (`id_categorie`)
+) ENGINE=InnoDB ;
+
+-- On crée la table spécialité avec une clé étrangère de la table catégorie
+CREATE TABLE IF NOT EXISTS `specialite` (
+    `id_specialite` INT NOT NULL UNIQUE AUTO_INCREMENT, 
+    `nom_specialite` VARCHAR(30) NOT NULL UNIQUE, 
+    `id_categorie` INT NOT NULL, 
+    PRIMARY KEY (`id_specialite`), 
+    FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`)
+) ENGINE=InnoDB ;
+
+-- On crée la table artisan du mois, en indiquant par défaut que la valeur pour 'artisan du mois' est "FAUX". 
+CREATE TABLE IF NOT EXISTS `artisan_mois` (
+    `id_artisan_mois` INT NOT NULL UNIQUE AUTO_INCREMENT, 
+    `est_artisan_mois` BOOLEAN NOT NULL DEFAULT FALSE, 
+    PRIMARY KEY (`id_artisan_mois`)
+) ENGINE=InnoDB ;
+
+-- On crée enfin la table artisan avec toutes les foreign key qui le relient aux autres tables
+CREATE TABLE IF NOT EXISTS `artisan` (
+    `id_artisan` INT NOT NULL UNIQUE AUTO_INCREMENT, 
+    `nom_artisan` VARCHAR(45) NOT NULL, 
+    `note_artisan` DECIMAL (1,1), 
+    `a_propos_artisan` VARCHAR(250), 
+    `email_artisan` VARCHAR(45) NOT NULL UNIQUE, 
+    `site_web_artisan` VARCHAR(150) UNIQUE, 
+    `id_specialite` INT NOT NULL, 
+    `id_ville` INT NOT NULL, 
+    `id_artisan_mois` INT NOT NULL, 
+    PRIMARY KEY (`id_artisan`), 
+    FOREIGN KEY (`id_specialite`) REFERENCES `specialite`(`id_specialite`), 
+    FOREIGN KEY (`id_ville`) REFERENCES `ville`(`id_ville`), 
+    FOREIGN KEY (`id_artisan_mois`) REFERENCES `artisan_mois`(`id_artisan_mois`)
+) ENGINE=InnoDB ;
